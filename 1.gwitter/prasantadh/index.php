9c24163628297115cbcdc134db7a5b57ba122d47
prasant adhikari <pa1038@nyu.edu>
2023-06-03 15:15:24 -0400

    gwitter: add signin form

---
+++ 1.gwitter/prasantadh/index.php
@@ -0,0 +1,18 @@
+<html>
+
+<head>
+    <title> Gwitter </title>
+</head>
+
+<body>
+    Welcome to Gwitter! <br/>
+    <form action="signin.php" method="post">
+        <input type="text" name="username" placeholder="username"/>
+        <input type="text" name="password" placeholder="password"/>
+        <input type="submit" value="Submit">
+    </form>
+</body>
+
+</html>
+
+
